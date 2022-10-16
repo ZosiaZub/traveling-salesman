9bbf82bcb1b04561b9f5d6527a2573cf941fe340
zosia <zosia.zub@gmail.com>
2022-10-16 21:20:38 +0200

    Przerobione z gita, to, co ma Mikołaj

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 from sys import maxsize
-vertexes = 4
-edges = [[-1, 12, 13, 11], [12, -1, 14, 15], [13, 14, -1, 16], [11, 15, 16, -1]]
-start_node = 0
-path = [start_node]
+from itertools import permutations
+
+nr_of_vertexes = 4
 
 
-def last_path(node, cost, start_node, min_cost, shortest_path):
-    cost += edges[node][start_node]
-    path.append(node)
-    if cost < min_cost:
-        return shortest_path[cost, path]
-    return
+def findShortestPath(graph, s_n):
+    vertex = []
+    for v in range(nr_of_vertexes):
+        if v != s_n:
+            vertex.append(v)
+
+    min_path = maxsize
+    next_permutation = permutations(vertex)
+
+    for current_permutation in next_permutation:
+        current_cost = 0
+
+        start_vertex = s_n
+        for end_vertex in current_permutation:
+            current_cost += graph[start_vertex][end_vertex]
+            start_vertex = end_vertex
+        current_cost += graph[start_vertex][s_n]
+
+        min_path = min(min_path, current_cost)
+
+    return min_path
 
 
-def choose_path(node, cost, start_node, min_cost, shortest_path):
-    for v in range(vertexes-1):
-        if node != v:
-            cost += edges[node][v]
-            path.append(node)
-            if len(path) == vertexes - 1:
-                shortest_path = last_path(node, cost, start_node, min_cost, shortest_path)
-                cost = 0
-                path.clear()
-                path.append(start_node)
-                break
-            else:
-                choose_path(v, cost, start_node, min_cost, shortest_path)
-    return shortest_path
 
 if __name__ == "__main__":
-    shortest_path = [0, 0]
-    min_cost = 0
-    cost = 0
-    shortest_path = choose_path(start_node, cost, start_node, min_cost, shortest_path)
-    # print('waga najkrotszej sciezki wynosi: ' + shortest_path[0])
-    # print('kolejno odwiedzane wierzcholki: ' + shortest_path[1])
+
+    edges = [[0, 10, 15, 20], [10, 0, 35, 25],
+             [15, 35, 0, 30], [20, 25, 30, 0]]
+    start_node = 0
+    print(findShortestPath(edges, start_node))
