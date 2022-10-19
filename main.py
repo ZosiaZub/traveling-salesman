09a4da23b419dc09aeee2ef9ebbfea0abef03524
zosia <zosia.zub@gmail.com>
2022-10-19 17:14:49 +0200

    Moje, dużo po Mikołajowym, ale zwraca za małą wartość i bez powrotnego wierzchołka

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
-import fileinput
 from itertools import permutations
+from sys import maxsize
 
-#nr_of_vertexes = fileinput.input("moje_dane.txt")
+
+nr_of_vertexes = open("moje_dane.txt", "r").readline()
 
 
-def costOfEdges(file_name):
-    file = open(file_name, "r")
-    lines = file.read().splitlines()
+def costOfEdges(file):
+    file = open(file, "r")
     cost_of_edges = []
-    for i in range(1, len(lines)):
-        cost_of_edges.append(lines[i].split())
+    rows = file.read().splitlines()
+    nr_of_rows = len(rows)
+    for r in range(nr_of_rows-1):
+        cost_of_edges.append(rows[r+1].split())
     return cost_of_edges
 
 
-def nrOfVertexes(file_name):
-    file = open(file_name, "r")
-    return file.readline()
-
-
-def allRouts(start_node, nr_of_vertexes):
+def allPaths(nr_of_v):
     vertexes = []
-    for node in range(int(nr_of_vertexes)):
-        if node != start_node:
-            vertexes.append(node)
+    for v in range(int(nr_of_v)):
+        vertexes.append(v)
     return permutations(vertexes)
 
 
-def findShortestRout(data, number_of_cities):
-    min_rout = 2147483647
-    all_routs = allRouts(0, number_of_cities)
-    for route in all_routs:
+def findCheapestPath(cost_of_edges):
+    min_cost = maxsize
+    cheapest_path = []
+    all_paths = allPaths(nr_of_vertexes)
+    for path in all_paths:
         cost = 0
-        i = 0
-        for vertex in route:
-            cost += int(data[i][vertex])
-            i = vertex
+        start_vertex = 0
+        for end_vertex in path:
+            cost += int(cost_of_edges[start_vertex][end_vertex])
+            start_vertex = end_vertex
 
-        cost += int(data[i][0])
-        if cost < min_rout:
-            min_rout = cost
-    print(min_rout)
+        cost += int(cost_of_edges[start_vertex][0])
+        if cost < min_cost:
+            min_cost = cost
+            cheapest_path = path
+    print("Min cost: " + str(min_cost))
+    print("for path: " + str(cheapest_path))
 
 
 if __name__ == "__main__":
-    findShortestRout(costOfEdges("moje_dane.txt"), nrOfVertexes("moje_dane.txt"))
+    findCheapestPath(costOfEdges("tsp_12.txt"))
+
+#
+#
+# if __name__ == "__main__":
+#     findShortestRout(costOfEdges("moje_dane.txt"), nrOfVertexes("moje_dane.txt"))
+#     # findShortestRout(costOfEdges("tsp_6_1.txt"), nrOfVertexes("tsp_6_1.txt"))
+# findShortestRout(costOfEdges("tsp_13.txt"), nrOfVertexes("tsp_13.txt"))
