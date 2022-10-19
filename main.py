e940179bf7e74075117d1bfd93c770a79cb69e8a
zosia <zosia.zub@gmail.com>
2022-10-19 19:36:30 +0200

    Moje, dużo po Mikołajowym, ale zwraca za małą wartość i bez powrotnego wierzchołka

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ from itertools import permutations
 from sys import maxsize
 
 
-nr_of_vertexes = open("moje_dane.txt", "r").readline()
+nr_of_vertexes = int(open("tsp_6_1.txt", "r").readline())
 
 
 def costOfEdges(file):
     file = open(file, "r")
     cost_of_edges = []
     rows = file.read().splitlines()
-    nr_of_rows = len(rows)
-    for r in range(nr_of_rows-1):
-        cost_of_edges.append(rows[r+1].split())
+    for r in range(1, nr_of_vertexes + 1):
+        cost_of_edges.append(rows[r].split())
     return cost_of_edges
 
 
 def allPaths(nr_of_v):
     vertexes = []
-    for v in range(int(nr_of_v)):
+    for v in range(1, int(nr_of_v)):
         vertexes.append(v)
     return permutations(vertexes)
 
@@ -32,21 +31,18 @@ def findCheapestPath(cost_of_edges):
         for end_vertex in path:
             cost += int(cost_of_edges[start_vertex][end_vertex])
             start_vertex = end_vertex
-
         cost += int(cost_of_edges[start_vertex][0])
+
         if cost < min_cost:
             min_cost = cost
-            cheapest_path = path
+            cheapest_path.clear()
+            cheapest_path.append(0)
+            cheapest_path.extend(list(path))
+            cheapest_path.append(0)
+
     print("Min cost: " + str(min_cost))
     print("for path: " + str(cheapest_path))
 
 
 if __name__ == "__main__":
-    findCheapestPath(costOfEdges("tsp_12.txt"))
-
-#
-#
-# if __name__ == "__main__":
-#     findShortestRout(costOfEdges("moje_dane.txt"), nrOfVertexes("moje_dane.txt"))
-#     # findShortestRout(costOfEdges("tsp_6_1.txt"), nrOfVertexes("tsp_6_1.txt"))
-# findShortestRout(costOfEdges("tsp_13.txt"), nrOfVertexes("tsp_13.txt"))
+    findCheapestPath(costOfEdges("tsp_6_1.txt"))
